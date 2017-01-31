44a7ec30957f3d60d7e1c64d0839b10771c67983
Mathieu Oreopoulos Wilson <maorea@l4714-13.info.polymtl.ca>
2017-01-31 17:25:45 -0500

    basic CS et QS

---
+++ TP1/src/tp1/inf4705/CountingSort.java
@@ -0,0 +1,66 @@
+package tp1.inf4705;
+
+public class CountingSort 
+{
+
+	private CountingSort()
+	{
+		
+	}
+	
+	private static final CountingSort instance = new CountingSort();
+
+	
+	public static CountingSort getInstance()
+	{
+		return instance;
+	}
+
+	//Méthode tirée de : http://www.geeksforgeeks.org/counting-sort/
+
+	private int[] sort(int[] array)
+	{
+		
+	    int[] aux = new int[array.length];
+	    
+	    // find the smallest and the largest value
+	    int min = array[0];
+	    int max = array[0];
+	    for (int i = 1; i < array.length; i++) {
+	      if (array[i] < min) {
+	        min = array[i];
+	      } else if (array[i] > max) {
+	        max = array[i];
+	      }
+	    }
+	 
+	    // init array of frequencies
+	    int[] counts = new int[max - min + 1];
+	 
+	    // init the frequencies
+	    for (int i = 0;  i < array.length; i++) {
+	      counts[array[i] - min]++;
+	    }
+	 
+	    // recalculate the array - create the array of occurences
+	    counts[0]--;
+	    for (int i = 1; i < counts.length; i++) {
+	      counts[i] = counts[i] + counts[i-1];
+	    }
+	 
+	    /*
+	      Sort the array right to the left
+	      1) Look up in the array of occurences the last occurence of the given value
+	      2) Place it into the sorted array
+	      3) Decrement the index of the last occurence of the given value
+	      4) Continue with the previous value of the input array (goto set1), 
+	         terminate if all values were already sorted
+	    */ 
+	    for (int i = array.length - 1; i >= 0; i--) {
+	        aux[counts[array[i] - min]--] = array[i];
+	    }
+	 
+	    return aux;
+		
+	}
+}
